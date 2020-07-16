20bc8cf4d265f790f83c716b13e4a33b13dbd609
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-16 16:45:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbow(colors)
-  puts "colors: #{color[0]}, #{color[1]}, #{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6]},"
+  puts "colors: #{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]},"
    display rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 end
